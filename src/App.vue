8e066946745c33a80315d3d36e6e58e4c84bf127
GOZU Kenichiro <gozuk16@gmail.com>
2023-07-16 16:57:36 +0900

    sample_vuetify3からGantt部分を抜き出した

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+   <v-app>
+      <v-app-bar color="grey-lighten-2">
+        Appバー
+        <v-spacer />
+        <v-divider vertical />
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn text v-bind="props">foo</v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-btn text>bar</v-btn>
+        <v-divider vertical />
+        <v-btn text>info</v-btn>
+      </v-app-bar>
+      <v-main>
+        <ApexCharts />
+        <Sample />
+      </v-main>
+    </v-app>
+
+</template>
+
+<script setup>
+import ApexCharts from './components/ApexCharts.vue'
+import Sample from './components/Sample.vue'
+</script>
+
+<script>
+  export default {
+    components: {
+      ApexCharts,
+    },
+    data: () => ({
+      items: [
+        { title: 'Menu1' },
+        { title: 'Menu2' },
+        { title: 'Menu3' },
+        { title: 'Menu4' },
+      ],
+      closeOnContentClick: true,
+    }),
+  }
+</script>
